003342ad90937a0270cf6af43cee3b9747547166
sayalimohite55 <sayalimohite55@gmail.com>
2025-03-01 23:52:03 +0530

    Implemented solution to identify max area container to trap most water

---
+++ src/com/problems/arraysAndStrings/ContainerWithMostWater.java
@@ -0,0 +1,63 @@
+package com.problems.arraysAndStrings;
+
+/*
+* Question: Container With Most Water
+* You are given an integer array height of length n.
+* There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0)
+* and (i, height[i]).
+* Find two lines that together with the x-axis form a container,
+* such that the container contains the most water.
+* Return the maximum amount of water a container can store.
+* Notice that you may not slant the container.
+* */
+public class ContainerWithMostWater {
+    private static int getMaxAreaBruteForceSolution(int height[]) {
+        /*
+         * Intent : Identify area for every pair of elements and take maxArea
+         * */
+
+        int maxArea = 0;
+        for(int i=0;i<height.length; i++) {
+            for(int j=i+1;j<height.length; j++) {
+                int width = j-1;
+                maxArea = Math.max(maxArea, Math.min(height[i],height[j]) * width);
+            }
+        }
+        return maxArea;
+        /*
+         * Time Complexity = o(n^2)
+         * Space Complexity = o(1)
+         * */
+    }
+
+    private static int getMaxAreaOptimised(int height[]) {
+        /*
+        * Intent : Identify area using two pinter technique for any given nodes and take maxArea
+        * */
+        int left = 0, right = height.length-1;
+        int maxArea = 0;
+        while(left < right) {
+            int width = right - left;
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * width);
+            if(height[left] <= height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxArea;
+        /*
+        * Time Complexity = o(n)
+        * Space Complexity = o(1)
+        * */
+    }
+
+    public static void main(String[] args) {
+        int[] height = new int[]{1,8,6,2,5,4,8,3,7};
+
+        //brute force solution - o(n^2)
+        System.out.println("Max Area: "+ getMaxAreaBruteForceSolution(height));
+
+        //optimised solution - o(n)
+        System.out.println("Max Area: "+ getMaxAreaOptimised(height));
+    }
+}
