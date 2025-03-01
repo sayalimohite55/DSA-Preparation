18f68e87c923405b53fa056f9d25a0165b380a12
sayalimohite55 <sayalimohite55@gmail.com>
2025-03-01 23:14:41 +0530

    Implemented different ways to get median of two sorted arrays

--- src/com/problems/arraysAndStrings/MedianOfTwoSortedArrays.java
+++ src/com/problems/arraysAndStrings/MedianOfTwoSortedArrays.java
@@ -17,23 +17,14 @@ public class MedianOfTwoSortedArrays {
         int length = nums1.length + nums2.length;
         int[] arr = new int[length];
         int i=0, j=0, k=0;
-        while(i<nums1.length && j<nums2.length) {
-            if (i < nums1.length && nums1[i] < nums2[j]) {
+        while(i != nums1.length || j != nums2.length) {
+            if (i != nums1.length && j != nums2.length ) {
+                arr[k++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+            } else if (i != nums1.length) {
                 arr[k++] = nums1[i++];
-            } else if (j < nums2.length && nums1[i] > nums2[j]) {
-                arr[k++] = nums2[j++];
             } else {
-                arr[k++] = nums1[i++];
                 arr[k++] = nums2[j++];
             }
-        }
-
-        while(i<nums1.length){
-            arr[k++] = nums1[i++];
-        }
-
-        while(j<nums2.length){
-            arr[k++] = nums2[j++];
         }
 
         int mid = length/2;
