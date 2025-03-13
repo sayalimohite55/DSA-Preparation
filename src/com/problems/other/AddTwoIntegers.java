baed3ef8ad37cca2178e32adade72109c7db0249
sayalimohite55 <sayalimohite55@gmail.com>
2025-03-13 15:34:43 +0530

    Implemented a function that returns sum of two numbers

---
+++ src/com/problems/other/AddTwoIntegers.java
@@ -0,0 +1,18 @@
+package com.problems.other;
+
+/*
+* Question:
+* Given two integers num1 and num2, return the sum of the two integers.
+* */
+public class AddTwoIntegers {
+    private static int sum(int n1, int n2) {
+        return n1+n2;
+        /*
+        * Time Complexity = o(1)
+        * Space Complexity = o(1)
+        * */
+    }
+    public static void main(String[] args) {
+        System.out.println("Addition: "+sum(5,7));
+    }
+}
